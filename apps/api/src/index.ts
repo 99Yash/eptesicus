309d7fdac37e4362309c7b5615f0f7cd7e5cdb82
yash <yashgouravkar@gmail.com>
2025-06-29 09:40:30 +0530

    api env

---
+++ apps/api/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { env } from './env';
+
+async function main() {
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+  app.listen(env.API_SERVER_PORT, () => {
+    console.log(`Server is running on port ${env.API_SERVER_PORT}`);
+  });
+}
+
+main();
