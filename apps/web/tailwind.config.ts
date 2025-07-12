e9c2c4c9b82f0e0c130c375e066de254166e07f9
yash <yashgouravkar@gmail.com>
2025-07-12 11:13:32 +0530

    www

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,17 +1,8 @@
 import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
 
 import web_config from '@workspace/tailwind-config';
 
 export default {
-  content: [...web_config.content, '../../packages/ui/src/*.{ts,tsx}'],
   presets: [web_config],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
-        mono: ['var(--font-geist-mono)', ...fontFamily.mono],
-      },
-    },
-  },
+  content: [...web_config.content, '../../packages/ui/src/*.{ts,tsx}'],
 } satisfies Config;
