c1d09191f7b87fbcf993b9e4a37aa44ce7ce33e1
yash <yashgouravkar@gmail.com>
2025-07-12 16:31:50 +0530

    db

---
+++ packages/db/src/config.ts
@@ -0,0 +1,14 @@
+import { type Config } from 'drizzle-kit';
+import { env } from './env';
+
+export default {
+  schema: './src/server/db/schema/index.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ['*'],
+  verbose: true, // Print all statements
+  // Always ask for confirmation
+  strict: true,
+} satisfies Config;
