56a7aed9dfa59faa9eb53409fc4495903a16175e
yash <yashgouravkar@gmail.com>
2025-07-23 11:35:40 +0530

    www

--- apps/api/src/lib/email.ts
+++ apps/api/src/lib/email.ts
@@ -1,13 +1,22 @@
 import { Resend } from 'resend';
 import { env } from '../env';
+import { AppError } from './error';
 
 const resend = new Resend(env.RESEND_API_KEY);
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-  const res = await resend.emails.send({
-    from: 'Eptesicus <yashgouravkar@gmail.com>',
-    to: [to],
-    subject: subject,
-    html: html,
-  });
+  try {
+    await resend.emails.send({
+      from: 'Eptesicus <yashgouravkar@gmail.com>',
+      to: [to],
+      subject: subject,
+      html: html,
+    });
+  } catch (error) {
+    throw new AppError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: `Failed to send email to ${to}: Subject: ${subject}`,
+      cause: error,
+    });
+  }
 };
