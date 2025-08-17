3f2c1024d4363207f1dadac2e189cb28267a20fd
yash <yashgouravkar@gmail.com>
2025-08-17 08:22:36 +0530

    qwqw

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,6 +4,22 @@ const nextConfig = {
   experimental: {
     browserDebugInfoInTerminal: process.env.NODE_ENV === 'development',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'framerusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- apps/web/src/components/organizations/organization-switcher.tsx
+++ apps/web/src/components/organizations/organization-switcher.tsx
@@ -11,6 +11,7 @@ import {
 } from '@workspace/ui/components/dropdown-menu';
 import { cn } from '@workspace/ui/lib/utils';
 import { Building2, Check, ChevronsUpDownIcon, Plus } from 'lucide-react';
+import Image from 'next/image';
 import { useState } from 'react';
 import { useOrganization } from '~/components/layouts/organization-provider';
 
@@ -105,10 +106,12 @@ export function OrganizationSwitcher({
           >
             <div className="flex flex-1 items-start gap-2">
               {org.logo_url ? (
-                <img
+                <Image
                   src={org.logo_url}
                   alt={org.name}
                   className="rounded self-center border border-main-muted size-6"
+                  width={24}
+                  height={24}
                 />
               ) : (
                 <Building2 className="h-4 w-4 text-muted-foreground mt-0.5 flex-shrink-0" />
